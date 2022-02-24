7a6d9c0c7cf385abbf00a40c320af8398bd6fe44
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-02-24 14:32:28 -0300

    :construction: Create Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
+import Note from "./Note"
 
 function App() {
     return (
         <div>
             <Header />
+            <Note title='titulo' content='conteudo' />
             <Footer />
         </div>
     )
