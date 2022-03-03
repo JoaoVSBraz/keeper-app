e951e0b94d037bbe833d99730829c1a1f444a313
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-03-03 10:33:38 -0300

    :construction: Create CreateArea component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,13 @@ import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
 import notes from '../notes'
+import CreateArea from "./CreateArea"
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(note => <Note key={note.key} title={note.title} content={note.content}/>)}
+            <CreateArea />
             <Footer />
         </div>
     )
